038ee2e6ad5e750036d9193206334c05a0bed637
Sebastian Rivera <sebastian-010198@hotmail.com>
2020-12-01 16:58:41 -0500

    add inter font

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './components/app/app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MainNavComponent } from './components/main-nav/main-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainNavComponent
   ],
   imports: [
     BrowserModule
