33270daaccd309429ec7c5eda7982985fadd2ee8
Sebastian Rivera <sebastian-010198@hotmail.com>
2020-12-12 20:14:52 -0500

    create active function for select item

--- src/app/components/content/content.component.ts
+++ src/app/components/content/content.component.ts
@@ -9,6 +9,28 @@ export class ContentComponent implements OnInit {
 
   constructor() { }
 
+  selected: boolean = false;
+  element: any = null;
+
+  active(e: any) {
+    console.log(this.element);
+
+    if (this.element == e.target) {
+      console.log(true);
+    } else {
+      console.log(false);
+    }
+    this.element = e.target;
+    this.selected = !this.selected;
+  }
+
+  classes() {
+    return {
+      'content__item': true,
+      'active': this.selected
+    }
+  }
+
   ngOnInit(): void {
   }
 
