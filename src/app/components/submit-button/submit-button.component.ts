d0be69af969975be4996f8bf15b53485c2df6530
Sebastian Rivera <sebastian-010198@hotmail.com>
2020-12-21 21:46:44 -0500

    fix scss in banner components

--- src/app/components/submit-button/submit-button.component.ts
+++ src/app/components/submit-button/submit-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-submit-button',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./submit-button.component.scss']
 })
 export class SubmitButtonComponent implements OnInit {
+
+  @Input() title: string = '';
 
   constructor() { }
 
